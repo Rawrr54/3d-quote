a36917d05b706a2519948ee1918bc89cf2b1af8f
rawrr54 <wilfridngo@gmail.com>
2017-07-10 17:56:03 -0400

    added route /quotes and /reports, and created a templates for the qouting page 'quote.gohtml'

--- main.go
+++ main.go
@@ -18,8 +18,37 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/quote", quote)
+	http.HandleFunc("/reports", reports)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
+}
+
+func quote(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		ext := strings.Split(fh.Filename, ".")[1]
+		fname := strings.Split(fh.Filename, ".")[0] + "." + ext
+		//create new file
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "3dfiles", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+	}
+	tpl.ExecuteTemplate(w, "quote.gohtml", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
@@ -48,4 +77,8 @@ func index(w http.ResponseWriter, req *http.Request) {
 		io.Copy(nf, mf)
 	}
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func reports(w http.ResponseWriter, req *http.Request) {
+
 }
